241a5c2ef0833e379a14fdb462488b8e69d94e7a
pragna88 <pragna88@users.noreply.github.com>
2020-01-18 15:50:07 -0500

    User regestration

--- config/db.js
+++ config/db.js
@@ -6,10 +6,11 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useCreateIndex: true
     });
 
-    console.log('MongoDB connected...');
+    console.log('MongoDB conected...');
   } catch (err) {
     console.error(err.message);
     //Exit process with failure
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-//connect Database
+
+//Connect Database
+
 connectDB();
 
 //Init Middleware
@@ -10,12 +12,12 @@ app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
-//Define routes
+//Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
