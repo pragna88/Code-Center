5b9afdc024f6e2dd6a2a62c211fb73c8af69ecf2
pragna88 <pragna88@users.noreply.github.com>
2020-01-21 01:44:56 -0500

    Minor Changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
       </h1>
       <ul>
         <li>
-          <a href='!#'>Developers</a>
+          <a href='/!#'>Developers</a>
         </li>
         <li>
           <Link to='/register'>Register</Link>
