83a2e0e81cd4d9f056b4acb844a24cc591d2d47f
pragna88 <pragna88@users.noreply.github.com>
2020-01-23 03:09:02 -0500

    Created get current profie using token from DB

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -18,13 +18,13 @@ export const loadUser = () => async dispatch => {
   }
 
   try {
-    const res = await axios.get('api/auth');
+    const res = await axios.get('/api/auth');
 
     dispatch({
       type: USER_LOADED,
       payload: res.data
     });
-    dispatch(loadUser());
+    // dispatch(loadUser());
   } catch (err) {
     dispatch({
       type: AUTH_ERROR
@@ -42,7 +42,7 @@ export const register = ({ name, email, password }) => async dispatch => {
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post('api/users', body, config);
+    const res = await axios.post('/api/users', body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
@@ -70,12 +70,13 @@ export const login = (email, password) => async dispatch => {
   const body = JSON.stringify({ email, password });
 
   try {
-    const res = await axios.post('api/auth', body, config);
+    const res = await axios.post('/api/auth', body, config);
 
     dispatch({
       type: LOGIN_SUCCESS,
       payload: res.data
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
