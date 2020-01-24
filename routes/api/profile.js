25a288170f09f105cd686c4ba5fe1e0417b1a964
pragna88 <pragna88@users.noreply.github.com>
2020-01-23 22:21:30 -0500

    Created delete functions for the account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 router.get('/me', auth, async (req, res) => {
   try {
@@ -142,7 +143,8 @@ router.get('/user/:user_id', async (req, res) => {
 //@access Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - Remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
